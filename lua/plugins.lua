e8a17a6e760ae4e8e79612df84075ecbd94f606b
redflame2112 <sagnik3@illinois.edu>
2024-04-09 01:21:49 -0500

    feat: Added support for Mason

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -280,20 +280,46 @@ return {
  
      dependencies = { 'MunifTanjim/nui.nvim' },
   },
- -- { 
- --   'codota/tabnine-nvim', 
- --   build = "./dl_binaries.sh",
- --   config = function ()
- --     require('tabnine').setup({
- --       disable_auto_comment=true,
- --       accept_keymap="<Tab>",
- --       dismiss_keymap = "<C-]>",
- --       debounce_ms = 800,
- --       suggestion_color = {gui = "#808080", cterm = 244},
- --       exclude_filetypes = {"TelescopePrompt", "NvimTree"},
- --       log_file_path = nil, -- absolute path to Tabnine log file
- --     })
- --   end
- -- },
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+    opts = {
+      ensure_installed = {}, -- not an option from mason.nvim
+      ui = {
+        icons = {
+          package_pending = " ",
+          package_installed = "󰄳 ",
+          package_uninstalled = " 󰚌",
+        },
+      },
+      max_concurrent_installers = 10,
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require "mason-registry"
+      mr:on("package:install:success", function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load this newly installed LSP server
+          require("lazy.core.handler.event").trigger {
+            event = "FileType",
+            buf = vim.api.nvim_get_current_buf(),
+          }
+        end, 100)
+      end)
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
 }
 
