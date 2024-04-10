4bf773d515cd25048ae1a73bb378128ceea94811
redflame2112 <sagnik3@illinois.edu>
2024-04-10 01:09:17 -0500

    feat: I got GPT :D

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,20 @@
 --local highlight = as.highlight
 
+local GPT_WELCOME_MESSAGE = [[
+                                       _____
+                                      /   __)\   
+                                      |  /  \ \
+                                   ___|  |__/ /
+                                  / (_    _)_/   Ask and you shall receive
+                                 / /  |  |       
+                                 \ \__/  | 
+                                  \(_____/    
+                               
+]]
+
+
+
+
 return {
   {
     'nvim-lualine/lualine.nvim',
@@ -368,5 +383,50 @@ return {
         require("codeium").setup({
         })
     end
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+          api_key_cmd = "echo $OPENAI_API_KEY",
+          edit_with_instructions = {
+          toggle_diff = "<C-d>"
+        },
+        chat = {
+          welcome_message = GPT_WELCOME_MESSAGE,
+        },
+        keymaps = {
+          close = "<C-c>",
+          yank_last = "<C-y>",
+          yank_last_code = "<C-k>",
+          scroll_up = "<C-u>",
+          scroll_down = "<C-d>",
+          new_session = "<C-n>",
+          cycle_windows = "<Tab>",
+          cycle_modes = "<C-f>",
+          next_message = "<C-j>",
+          prev_message = "<C-k>",
+          select_session = "<Space>",
+          rename_session = "r",
+          delete_session = "d",
+          draft_message = "<C-r>",
+          edit_message = "e",
+          delete_message = "d",
+          toggle_settings = "<C-o>",
+          toggle_sessions = "<C-p>",
+          toggle_help = "<C-h>",
+          toggle_message_role = "<C-r>",
+          toggle_system_role_open = "<C-s>",
+          stop_generating = "<C-x>",
+        },
+      })
+    end,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim"
+    }
   },
 }
